095ca384cface8d927506ce491c9c89e1ff1b38d
Tran Dinh Quy <124875024+dqez@users.noreply.github.com>
2025-12-13 20:42:37 +0700

    Add day05 routing and Link

---
+++ day05/my-app/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-4">Contact</h1>
+      <p>Please contact us via email: quysprolegend@g .com</p>
+    </div>
+  );
+}
