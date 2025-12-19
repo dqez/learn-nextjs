f2af8e67af4d180ec10ec109fbee4a00af1844bb
Tran Dinh Quy <124875024+dqez@users.noreply.github.com>
2025-12-19 09:53:37 +0700

    Done

--- day11/my-app/app/api/info/route.ts
+++ day11/my-app/app/api/info/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server"
+import { brotliDecompress } from "zlib";
 
 export async function GET() {
   const data = {
@@ -8,4 +9,17 @@ export async function GET() {
   }
 
   return NextResponse.json(data);
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  //do something like, save to database,...
+  console.log("Data received: ", body);
+
+  return NextResponse.json({
+    message: "Data received!",
+    receivedData: body
+  }, { status: 201 });
+
 }
